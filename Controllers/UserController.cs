ee42e7bb09aa1ac591fdd602c8d7364abdb08755
Marie Floissac--Duforez <marie.floissac-duforez@epita.fr>
2021-04-30 17:02:10 +0100

    delete useless userController functions

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -9,7 +9,6 @@ using OOP_CA_Macintosh.Data;
 using OOP_CA_Macintosh.Services;
 using System.IdentityModel.Tokens.Jwt;
 using Microsoft.IdentityModel.Tokens;
-
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
@@ -30,112 +29,6 @@ namespace OOP_CA_Macintosh.Controllers
             _context = context;
         }
 
-        /*[AllowAnonymous]
-        [HttpPost("authenticate")]
-        public IActionResult Authenticate([FromBody] AuthenticateModel model)
-        {
-            var user = _userServices.Authenticate(model.Username, model.Password);
-            if (user == null)
-                return BadRequest(new { message = "Username or password is incorrect" });
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes(Configuration["Secret"]);
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(new Claim[]
-                {
-                     new Claim(ClaimTypes.Name, user.Id.ToString())
-                }),
-                Expires = DateTime.UtcNow.AddDays(7),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-            };
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-            var tokenString = tokenHandler.WriteToken(token);
-            // return basic user info and authentication token
-            return Ok(new
-            {
-                Id = user.Id,
-                Username = user.Username,
-                FirstName = user.FirstName,
-                LastName = user.LastName,
-                Token = tokenString
-            });
-        }
-        [AllowAnonymous]
-        [HttpPost("register")]
-        public IActionResult Register([FromBody] RegisterModel model)
-        {
-            // map model to entity
-            var user = _mapper.Map<User>(model);
-            try
-            {
-                // create user
-                _userServices.Create(user, model.Password);
-                var tokenHandler = new JwtSecurityTokenHandler();
-                var key = Encoding.ASCII.GetBytes(Configuration["Secret"]);
-                var tokenDescriptor = new SecurityTokenDescriptor
-                {
-                    Subject = new ClaimsIdentity(new Claim[]
-                    {
-                        new Claim(ClaimTypes.Name, user.Id.ToString())
-                    }),
-                    Expires = DateTime.UtcNow.AddDays(7),
-                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-                };
-                var token = tokenHandler.CreateToken(tokenDescriptor);
-                var tokenString = tokenHandler.WriteToken(token);
-                return Ok(new
-                {
-                    Id = user.Id,
-                    Username = user.Username,
-                    FirstName = user.FirstName,
-                    LastName = user.LastName,
-                    Token = tokenString
-                });
-            }
-            catch (AppException ex)
-            {
-                // return error message if there was an exception
-                return BadRequest(new { message = ex.Message });
-            }
-        }
-        [HttpGet]
-        public IActionResult GetAll()
-        {
-            var users = _userServices.GetAll();
-            var model = _mapper.Map<IList<User>>(users);
-            return Ok(model);
-        }
-        [HttpGet("{id}")]
-        public IActionResult GetById(int id)
-        {
-            var user = _userServices.GetById(id);
-            var model = _mapper.Map<User>(user);
-            return Ok(model);
-        }
-        [HttpPut("{id}")]
-        public IActionResult Update(int id, [FromBody] UpdateModel model)
-        {
-            // map model to entity and set id
-            var user = _mapper.Map<User>(model);
-            user.Id = id;
-            try
-            {
-                // update user 
-                _userServices.Update(user, model.CurrentPassword, model.NewPassword, model.ConfirmNewPassword);
-                return Ok();
-            }
-            catch (AppException ex)
-            {
-                // return error message if there was an exception
-                return BadRequest(new { message = ex.Message });
-            }
-        }
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
-        {
-            _userServices.Delete(id);
-            return Ok();
-        }*/
 
         [HttpGet("Login")]
         public IActionResult Login(string returnUrl)
