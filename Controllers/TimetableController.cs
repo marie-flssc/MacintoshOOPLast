29564c846863631e5847efd294d87322fa6cca6b
thib <tibeau031@gmail.com>
2021-05-02 21:25:05 +0100

    TimeTable Beautiful

--- Controllers/TimetableController.cs
+++ Controllers/TimetableController.cs
@@ -9,85 +9,149 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace OOP_CA_Macintosh.Controllers
 {
     [Authorize]
-    public class TimetableController : Controller
+   public class TimetableController : Controller
     {
         private readonly Context _context;
-        private readonly ILogger<TimetableController> _logger;
 
-        public TimetableController(Context context, ILogger<TimetableController> logger)
+        public TimetableController(Context context)
         {
             _context = context;
-            _logger = logger;
         }
-        public IActionResult Index()
+        [Authorize]
+        // GET: TimeTables
+        public async Task<IActionResult> Index()
         {
-            //var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            return View(_context.Timetable.ToList());
+            return View(await _context.Timetable.ToListAsync());
         }
 
-
-        [Authorize(Roles = AccessLevel.Faculty)]
-        public IActionResult SeeStudentTimetable(int? id)
+        // GET: TimeTables/Details/5
+        public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
 
-            var time = GetCourses(id);
-            if (time == null)
+            var timeTable = await _context.Timetable
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (timeTable == null)
             {
                 return NotFound();
             }
 
-            return View(time);
+            return View(timeTable);
         }
 
-        
-        [Authorize]
-        public IActionResult SeeOwnTimetable()
+        // GET: TimeTables/Create
+        public IActionResult Create()
         {
-            String userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            var user = _context.User.ToList().Find(x => x.Username.Equals(userId));
-            int id = user.Id;
-            var time = GetCourses(id);
-            if (time == null)
+            return View();
+        }
+
+        // POST: TimeTables/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Start,End,Text,Color")] StudentToClass timeTable)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(timeTable);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(timeTable);
+        }
+
+        // GET: TimeTables/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
             {
                 return NotFound();
             }
 
-            return View(time);
+            var timeTable = await _context.Timetable.FindAsync(id);
+            if (timeTable == null)
+            {
+                return NotFound();
+            }
+            return View(timeTable);
         }
 
-
-        public List<Courses> GetCourses(int? id)
+        // POST: TimeTables/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Start,End,Text,Color")] StudentToClass timeTable)
         {
-            List<Courses> res = new List<Courses> { };
-
-            var time = _context.Timetable;
-            if(id != null)
+            if (id != timeTable.Id)
             {
-                foreach (StudentToClass classe in time)
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
                 {
-                    if (classe.StudentId == id.Value)
+                    _context.Update(timeTable);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TimeTableExists(timeTable.Id))
                     {
-                        var tt = _context.Courses.ToList().Find(x => x.Id == classe.Course);
-                        res.Add(tt);
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
                     }
                 }
+                return RedirectToAction(nameof(Index));
             }
-            return res;
+            return View(timeTable);
         }
 
-        public JsonResult GetEvents()
+        // GET: TimeTables/Delete/5
+        public async Task<IActionResult> Delete(int? id)
         {
-            var user = _context.User.FirstOrDefault(x => x.Username == User.Identity.Name);
-            var events = _context.Timetable.Where(x => x.StudentId == user.Id);
-            return new JsonResult(new { Data = events });
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var timeTable = await _context.Timetable
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (timeTable == null)
+            {
+                return NotFound();
+            }
+
+            return View(timeTable);
+        }
+
+        // POST: TimeTables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var timeTable = await _context.Timetable.FindAsync(id);
+            _context.Timetable.Remove(timeTable);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TimeTableExists(int id)
+        {
+            return _context.Timetable.Any(e => e.Id == id);
         }
     }
 }
